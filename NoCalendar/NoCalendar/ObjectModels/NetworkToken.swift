2e96d6f2cad0770e4c744d4a943e11fd3398bfdd
Alexander Klonov <alexander.klonov@mail.ru>
2022-05-02 21:16:31 +0300

    Clean up trash

--- NoCalendar/NoCalendar/ObjectModels/NetworkToken.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkToken.swift
-//  NoCalendar
-//
-//  Created by Александр Клонов on 18.04.2022.
-//
-
-import Foundation
--- NoCalendar/NoCalendar/Presenters/SignleDayPresenter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SignleDayPresenter.swift
-//  NoCalendar
-//
-//  Created by Александр Клонов on 01.05.2022.
-//
-
-import Foundation
